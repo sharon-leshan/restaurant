01613aa7d08af111935b0244f833f765ce5f745e
sharon-leshan <maleos380@gmail.com>
2021-11-23 00:45:07 +0300

    updated file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Seo from '../components/seo';
+import { FaBlackTie } from "react-icons/fa"
+import SEO from '../components/seo';
 
 const IndexPage = () => (
 	<Layout>
-		<Seo title="Home" />
-		<h3>The Restaurant site</h3>
+		<SEO title="Home" />
+		<h3><FaBlackTie/> The Restaurant site</h3>
 	</Layout>
 );
 
