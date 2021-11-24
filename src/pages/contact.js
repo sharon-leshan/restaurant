73501646c09e73f535875bbd5be21bb123b2d703
sharon-leshan <maleos380@gmail.com>
2021-11-24 12:16:45 +0300

    adding image and page header

---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo'
+import {PageHeader} from '../utils'
+import contactImg from '../images/bcg/contactBcg.jpg'
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <SEO title="Contact"/>
+            <PageHeader img={contactImg}>
+                <h3>Contact Page</h3>
+            </PageHeader>
+        </Layout>
+    )
+}
+
+export default ContactPage
