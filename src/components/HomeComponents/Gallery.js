2c166c5a59eae3576a1764284323139430db894a
sharon-leshan <maleos380@gmail.com>
2021-11-25 01:24:22 +0300

    importing new files

--- src/components/HomeComponents/Gallery.js
+++ src/components/HomeComponents/Gallery.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
-import {useStaticQuery, graphql} from 'gatsby'
-import { styles, Section} from '../../utils'
-import Img from 'gatsby-image'
+import React from 'react';
+import styled from 'styled-components';
+import { useStaticQuery, graphql } from 'gatsby';
+import { styles, Section } from '../../utils';
+import Img from 'gatsby-image';
 
 const Gallery = () => {
-    const data = useStaticQuery(graphql`
-        {
-            img1: file(relativePath: {eq: "homeGallery/img-1.jpeg"}) {
-                childImageSharp {
-                    fluid(maxWidth: 500){
-                        ...GatsbyImageSharpFluid_tracedSVG
-                    }
-                }
-            }
-        }
-    `)
-    return <Img fluid={data.img1.childImageSharp.fluid} />
-}
+	const data = useStaticQuery(graphql`
+		{
+			img1: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
+				childImageSharp {
+					fluid(maxWidth: 500) {
+						...GatsbyImageSharpFluid_tracedSVG
+					}
+				}
+			}
+		}
+	`);
+	return <Img fluid={data.img1.childImageSharp.fluid} />;
+};
 
-export default Gallery
+export default Gallery;
