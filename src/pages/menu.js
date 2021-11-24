01777dbbd773554423fe104482a677b50c19b18d
sharon-leshan <maleos380@gmail.com>
2021-11-24 12:15:34 +0300

    adding page header and image

---
+++ src/pages/menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo'
+import { PageHeader} from '../utils'
+import menuImg from '../images/bcg/menuBcg.jpg'
+
+const MenuPage = () => {
+    return (
+        <Layout>
+            <SEO title="Menu"/>
+            <PageHeader img={menuImg} >
+                <h3>Menu Page</h3>
+            </PageHeader>
+        </Layout>
+    )
+}
+
+export default MenuPage
