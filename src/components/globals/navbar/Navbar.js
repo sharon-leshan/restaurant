010ce497edd825fd125462e364ed21bd6b1302de
sharon-leshan <maleos380@gmail.com>
2021-11-25 01:27:46 +0300

    importing navbar files

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -1,35 +1,35 @@
-import React, {Component} from 'react'
-import NavbarHeader from './NavbarHeader'
-import NavbarLinks from './NavbarLinks'
-import NavbarIcons from './NavbarIcons'
-import styled from 'styled-components'
+import React, { Component } from 'react';
+import NavbarHeader from './NavbarHeader';
+import NavbarLinks from './NavbarLinks';
+import NavbarIcons from './NavbarIcons';
+import styled from 'styled-components';
 
 class Navbar extends Component {
-    state = {
-        navbarOpen: false,
-    }
-    handleNavbar = () => {
-        this.setState(() => {
-           return{ navbarOpen: !this.state.navbarOpen}
-        })
-    }
-    render() {
-        return (
-            <nav>
-                <NavWrapper>
-                    <NavbarHeader handleNavbar={ this.handleNavbar} />
-                    <NavbarLinks navbarOpen={this.state.navbarOpen} />
-                    <NavbarIcons/>
-                </NavWrapper>
-            </nav>
-        )
-    }
+	state = {
+		navbarOpen: false,
+	};
+	handleNavbar = () => {
+		this.setState(() => {
+			return { navbarOpen: !this.state.navbarOpen };
+		});
+	};
+	render() {
+		return (
+			<nav>
+				<NavWrapper>
+					<NavbarHeader handleNavbar={this.handleNavbar} />
+					<NavbarLinks navbarOpen={this.state.navbarOpen} />
+					<NavbarIcons />
+				</NavWrapper>
+			</nav>
+		);
+	}
 }
 
 const NavWrapper = styled.nav`
-    @media (min-width: 768px) {
-        display: flex;
-        align-items: center;
-    }
-`
-export default Navbar
+	@media (min-width: 768px) {
+		display: flex;
+		align-items: center;
+	}
+`;
+export default Navbar;
