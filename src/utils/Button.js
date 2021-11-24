0a9217a5a94c58f7cec94addfb6a5a3161c59de2
sharon-leshan <maleos380@gmail.com>
2021-11-25 01:16:49 +0300

    styling

--- src/utils/Button.js
+++ src/utils/Button.js
@@ -1,31 +1,31 @@
-import styled from 'styled-components'
-import {styles} from '../utils'
+import styled from 'styled-components';
+import { styles } from '../utils';
 
 const BannerButton = styled.button`
-    display: block;
-    color: ${styles.colors.mainWhite};
-    background: transparent;
-    padding: 0.5rem 1rem;
-    text-transform: uppercase;
-    font-size: 1.5rem;
-    letter-spacing: 0.5rem;
-    font-weight: 700;
-    ${styles.border({color: `${styles.colors.mainWhite}`})};
-    margin-top: 1rem;
-    ${styles.transObject({})};
-    &:hover {
-        background: ${styles.colors.mainWhite};
-        color: ${styles.colors.mainBlack};
-        cursor: pointer;
-    }
-`
+	display: block;
+	color: ${styles.colors.mainWhite};
+	background: transparent;
+	padding: 0.5rem 1rem;
+	text-transform: uppercase;
+	font-size: 1.5rem;
+	letter-spacing: 0.5rem;
+	font-weight: 700;
+	${styles.border({ color: `${styles.colors.mainWhite}` })};
+	margin-top: 1rem;
+	${styles.transObject({})};
+	&:hover {
+		background: ${styles.colors.mainWhite};
+		color: ${styles.colors.mainBlack};
+		cursor: pointer;
+	}
+`;
 const SectionButton = styled(BannerButton)`
-    color: ${styles.colors.mainBlack};
-    ${styles.border({color: `${styles.colors.mainBlack}`})};
-    &:hover {
-        background: ${styles.colors.mainBlack};
-        color: ${styles.colors.mainYellow};
-        cursor: pointer;
-    }
-`
-export {BannerButton, SectionButton}
+	color: ${styles.colors.mainBlack};
+	${styles.border({ color: `${styles.colors.mainBlack}` })};
+	&:hover {
+		background: ${styles.colors.mainBlack};
+		color: ${styles.colors.mainYellow};
+		cursor: pointer;
+	}
+`;
+export { BannerButton, SectionButton };
