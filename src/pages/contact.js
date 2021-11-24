3de198f89e7c7bd941b13f875e1b15845cbab2f5
sharon-leshan <maleos380@gmail.com>
2021-11-25 01:22:52 +0300

    importing new files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
-import SEO from '../components/seo'
-import {PageHeader, Banner} from '../utils'
-import contactImg from '../images/bcg/contactBcg.jpg'
+import SEO from '../components/seo';
+import { PageHeader, Banner } from '../utils';
+import contactImg from '../images/bcg/contactBcg.jpg';
 
 const ContactPage = () => {
-    return (
-        <Layout>
-            <SEO title="Contact"/>
-            <PageHeader img={contactImg}>
-                <Banner title="contact us" subtitle="let's get in touch"/>
-            </PageHeader>
-        </Layout>
-    )
-}
+	return (
+		<Layout>
+			<SEO title="Contact" />
+			<PageHeader img={contactImg}>
+				<Banner title="contact us" subtitle="let's get in touch" />
+			</PageHeader>
+		</Layout>
+	);
+};
 
-export default ContactPage
+export default ContactPage;
