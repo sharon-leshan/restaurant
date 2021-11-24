4880457253945bf04ce6335d5d096ab7cdc58f67
sharon-leshan <maleos380@gmail.com>
2021-11-25 01:18:44 +0300

    updated files

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,17 @@
-import * as styles from './styles'
-import {HomeHeader, PageHeader} from './Header'
-import {Banner} from './Banner'
-import {BannerButton, SectionButton} from './Button'
-import {Section} from './Section'
-import {Title} from './Title'
- 
-export {SectionButton, Title, Section, BannerButton, Banner, HomeHeader, PageHeader, styles}
+import * as styles from './styles';
+import { HomeHeader, PageHeader } from './Header';
+import { Banner } from './Banner';
+import { BannerButton, SectionButton } from './Button';
+import { Section } from './Section';
+import { Title } from './Title';
+
+export {
+	SectionButton,
+	Title,
+	Section,
+	BannerButton,
+	Banner,
+	HomeHeader,
+	PageHeader,
+	styles,
+};
