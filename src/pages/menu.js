380d052a92976180d1c432b5d01c1c12cf9a4ba1
sharon-leshan <maleos380@gmail.com>
2021-11-25 01:22:04 +0300

    added banner and page header

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
-import SEO from '../components/seo'
-import { PageHeader, Banner} from '../utils'
-import menuImg from '../images/bcg/menuBcg.jpg'
+import SEO from '../components/seo';
+import { PageHeader, Banner } from '../utils';
+import menuImg from '../images/bcg/menuBcg.jpg';
 
 const MenuPage = () => {
-    return (
-        <Layout>
-            <SEO title="Menu"/>
-            <PageHeader img={menuImg} >
-                <Banner title="our menu" subtitle="The best in town" />
-            </PageHeader>
-        </Layout>
-    )
-}
+	return (
+		<Layout>
+			<SEO title="Menu" />
+			<PageHeader img={menuImg}>
+				<Banner title="our menu" subtitle="The best in town" />
+			</PageHeader>
+		</Layout>
+	);
+};
 
-export default MenuPage
+export default MenuPage;
