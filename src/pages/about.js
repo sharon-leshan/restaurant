8f1b23a98b4a327a84c533dc05e97f36facc39cc
sharon-leshan <maleos380@gmail.com>
2021-11-25 01:23:12 +0300

    importing new files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
-import SEO from '../components/seo'
-import { PageHeader, Banner} from '../utils'
-import aboutImg from '../images/bcg/aboutBcg.jpg'
+import SEO from '../components/seo';
+import { PageHeader, Banner } from '../utils';
+import aboutImg from '../images/bcg/aboutBcg.jpg';
 
 const AboutPage = () => {
-    return (
-        <Layout>
-            <SEO title="About"/>
-            <PageHeader img={aboutImg}>
-                <Banner title="about us" subtitle="a little about us" />
-            </PageHeader>
-        </Layout>
-    )
-}
+	return (
+		<Layout>
+			<SEO title="About" />
+			<PageHeader img={aboutImg}>
+				<Banner title="about us" subtitle="a little about us" />
+			</PageHeader>
+		</Layout>
+	);
+};
 
-export default AboutPage
+export default AboutPage;
