3759c82313f57f330cf9fa971d74f6030d9a8e86
sharon-leshan <maleos380@gmail.com>
2021-11-24 12:16:24 +0300

    adding image and page header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from '../components/layout';
-import { FaBlackTie } from "react-icons/fa"
+import {HomeHeader} from '../utils'
+import img from '../images/bcg/homeBcg.jpg'
 import SEO from '../components/seo';
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
-		<h3><FaBlackTie/> The Restaurant site</h3>
+		<HomeHeader img={img}>
+			<h3>The Restaurant Site</h3>
+		</HomeHeader>
 	</Layout>
 );
 
