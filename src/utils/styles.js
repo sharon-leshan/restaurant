33d558de0087cd91c8918028e346aa829de2837f
sharon-leshan <maleos380@gmail.com>
2021-11-24 21:19:38 +0300

    stylings

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -10,3 +10,10 @@ export const transDefault = 'transition:all 0.5s ease-in-out';
 export const transObject = ({property = 'all', time = '0.5s', type = 'ease-in-out'}) => {
     return `transition:${property} ${time} ${type}`
 }
+export const textSlanted = `font-family: 'Caveat', cursive;`
+export const letterSpacing = ({spacing = '0.1rem'}) => {
+    return `letter-spacing:${spacing}`
+}
+export const border = ({width = '0.15rem', type = 'solid', color = 'white'}) => {
+    return `border:${width} ${type} ${color}`
+}
