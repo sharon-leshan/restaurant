33ff5cb869c8f32dc5ee527962ab2a89538baaa5
sharon-leshan <maleos380@gmail.com>
2021-11-25 23:36:07 +0300

    importing about file

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout';
 import Seo from '../components/seo';
 import { PageHeader, Banner } from '../utils';
 import aboutImg from '../images/bcg/aboutBcg.jpg';
+import About from '../components/About/About'
 
 const AboutPage = () => {
 	return (
@@ -11,6 +12,7 @@ const AboutPage = () => {
 			<PageHeader img={aboutImg}>
 				<Banner title="about us" subtitle="a little about us" />
 			</PageHeader>
+			<About/>
 		</Layout>
 	);
 };
