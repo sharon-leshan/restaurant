a72b1e07c2f6db89135c086c1da07f6f936ba352
sharon-leshan <maleos380@gmail.com>
2021-11-25 17:06:35 +0300

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
 	siteMetadata: {
 		title: `Restaurant Site`,
@@ -7,23 +11,31 @@ module.exports = {
 	},
 	plugins: [
 		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-image`,		
+		`gatsby-plugin-image`,
 		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.SPACE_ID,
+				// Learn about environment variables: https://gatsby.dev/env-vars
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+			},
+		},
 		{
 			resolve: `gatsby-plugin-sass`,
 			options: {
-			  cssLoaderOptions: {
-				camelCase: false,
-			  },
+				cssLoaderOptions: {
+					camelCase: false,
+				},
 			},
-		  },
-		  
-		  {
+		},
+
+		{
 			resolve: `gatsby-plugin-sass`,
 			options: {
-			  implementation: require("node-sass"),
+				implementation: require('node-sass'),
 			},
-		  },
+		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
