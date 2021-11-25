cc98a17211819b1cd84fc324888c4a780769682c
sharon-leshan <maleos380@gmail.com>
2021-11-25 23:36:29 +0300

    importing contact file

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout';
 import Seo from '../components/seo';
 import { PageHeader, Banner } from '../utils';
 import contactImg from '../images/bcg/contactBcg.jpg';
+import Contact from '../components/Contact/Contact'
 
 const ContactPage = () => {
 	return (
@@ -11,6 +12,7 @@ const ContactPage = () => {
 			<PageHeader img={contactImg}>
 				<Banner title="contact us" subtitle="let's get in touch" />
 			</PageHeader>
+			<Contact/>
 		</Layout>
 	);
 };
