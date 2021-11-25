6e9283148cf9b1841d6f2c5c6d3dcf9b916514e2
sharon-leshan <maleos380@gmail.com>
2021-11-26 00:11:37 +0300

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,12 +11,41 @@ module.exports = {
 	},
 	plugins: [
 		{
+			resolve: `gatsby-plugin-robots-txt`,
+			options: {
+				host: `https://maleo-restaurant.netlify.app/`,
+				sitemap: `https://maleo-restaurant.netlify.app/sitemap.xml`,
+				policy: [{ userAgent: '*', allow: '/' }],
+			},
+		},
+		`gatsby-plugin-sitemap`,
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: `G-C5YNSJCNVK`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
+				trackingIds: [
+					'G-C5YNSJCNVK', // Google Analytics / GA
+				],
+				// This object is used for configuration specific to this plugin
+				pluginConfig: {
+					// Puts tracking script in the head instead of the body
+					head: true,
+				},
+			},
+		},
+		{
 			resolve: 'gatsby-plugin-snipcartv3',
 			options: {
-			  apiKey: process.env.SNIPCART_API,
-			  autopop: true
-			}
-		  },
+				apiKey: process.env.SNIPCART_API,
+				autopop: true,
+			},
+		},
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-image`,
 		{
