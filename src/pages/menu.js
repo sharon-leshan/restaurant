26b44fe910465a3254ea980b02c12a6156a7b446
sharon-leshan <maleos380@gmail.com>
2021-12-05 16:58:10 +0300

    menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,6 +3,8 @@ import Layout from '../components/layout';
 import Seo from '../components/seo';
 import { PageHeader, Banner } from '../utils';
 import menuImg from '../images/bcg/menuBcg.jpg';
+import Gallery from '../components/HomeComponents/Gallery';
+import Menu from '../components/HomeComponents/Menu';
 
 const MenuPage = () => {
 	return (
@@ -11,6 +13,8 @@ const MenuPage = () => {
 			<PageHeader img={menuImg}>
 				<Banner title="our menu" subtitle="The best in town" />
 			</PageHeader>
+			<Gallery />
+			<Menu />
 		</Layout>
 	);
 };
